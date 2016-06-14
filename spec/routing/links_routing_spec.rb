69925650e085a5fd040c7cfd9814d045954eeafd
Sam Blackman <sblackman@gmail.com>
2016-06-14 16:45:38 -0400

    add routing test for #reroute action

--- spec/routing/links_routing_spec.rb
+++ spec/routing/links_routing_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 RSpec.describe LinksController, type: :routing do
   describe "routing" do
 
+    it "routes to #reroute" do
+      expect(:get => "/somerandomlink").to route_to("links#reroute", :wildcard => "somerandomlink")
+    end
+
     it "routes to #index" do
       expect(:get => "/links").to route_to("links#index")
     end
