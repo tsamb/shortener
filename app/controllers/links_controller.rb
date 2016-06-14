14601740f7d45c2407c5d4882ff6c1962898972b
Sam Blackman <sblackman@gmail.com>
2016-06-14 13:49:22 -0400

    fix syntax errors

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -34,20 +34,18 @@ class LinksController < ApplicationController
 
   def create
     @link = Link.new(link_params)
-      if @link.save
-        redirect_to @link, notice: 'Link was successfully created.'
-      else
-        render :new
-      end
+    if @link.save
+      redirect_to @link, notice: 'Link was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-      if @link.update(link_params)
-        redirect_to @link, notice: 'Link was successfully updated.'
-      else
-        render :edit
-      end
+    if @link.update(link_params)
+      redirect_to @link, notice: 'Link was successfully updated.'
+    else
+      render :edit
     end
   end
 
