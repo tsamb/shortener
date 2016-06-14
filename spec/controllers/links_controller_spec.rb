37535550d5c615c953d7785cc16b3f5ed5265916
Sam Blackman <sblackman@gmail.com>
2016-06-14 14:52:39 -0400

    add tests for redirects when not logged in

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -33,128 +33,198 @@ RSpec.describe LinksController, type: :controller do
   end
 
   describe "GET #index" do
-    it "assigns all the current user's links as @links" do
-      link = Link.create! valid_attributes
-      unowned_link = Link.create! unowned_attributes
-      get :index, {}, valid_session
-      expect(assigns(:links)).to eq([link])
+    context "when logged in" do
+      it "assigns all the current user's links as @links" do
+        link = Link.create! valid_attributes
+        unowned_link = Link.create! unowned_attributes
+        get :index, {}, valid_session
+        expect(assigns(:links)).to eq([link])
+      end
+
+      it "does not assign every link in the database to @links" do
+        link = Link.create! valid_attributes
+        unowned_link = Link.create! unowned_attributes
+        get :index, {}, valid_session
+        expect(assigns(:links)).to_not eq(Link.all)
+      end
     end
 
-    it "does not assign every link in the database to @links" do
-      link = Link.create! valid_attributes
-      unowned_link = Link.create! unowned_attributes
-      get :index, {}, valid_session
-      expect(assigns(:links)).to_not eq(Link.all)
+    context "when not logged in" do
+      it "redirects to login" do
+        link = Link.create! valid_attributes
+        unowned_link = Link.create! unowned_attributes
+        get :index
+        expect(response).to redirect_to(login_url)
+      end
     end
   end
 
   describe "GET #show" do
-    it "assigns the requested link as @link" do
-      link = Link.create! valid_attributes
-      get :show, {:id => link.to_param}, valid_session
-      expect(assigns(:link)).to eq(link)
+    context "when logged in" do
+      it "assigns the requested link as @link" do
+        link = Link.create! valid_attributes
+        get :show, {:id => link.to_param}, valid_session
+        expect(assigns(:link)).to eq(link)
+      end
+    end
+
+    context "when not logged in" do
+      it "redirects to login" do
+        link = Link.create! valid_attributes
+        get :show, {:id => link.to_param}
+        expect(response).to redirect_to(login_url)
+      end
     end
   end
 
   describe "GET #new" do
-    it "assigns a new link as @link" do
-      get :new, {}, valid_session
-      expect(assigns(:link)).to be_a_new(Link)
+    context "when logged in" do
+      it "assigns a new link as @link" do
+        get :new, {}, valid_session
+        expect(assigns(:link)).to be_a_new(Link)
+      end
+    end
+
+    context "when not logged in" do
+      it "redirects to login" do
+        get :new
+        expect(response).to redirect_to(login_url)
+      end
     end
   end
 
   describe "GET #edit" do
-    it "assigns the requested link as @link" do
-      link = Link.create! valid_attributes
-      get :edit, {:id => link.to_param}, valid_session
-      expect(assigns(:link)).to eq(link)
+    context "when logged in" do
+      it "assigns the requested link as @link" do
+        link = Link.create! valid_attributes
+        get :edit, {:id => link.to_param}, valid_session
+        expect(assigns(:link)).to eq(link)
+      end
+    end
+
+    context "when not logged in" do
+      it "redirects to login" do
+        link = Link.create! valid_attributes
+        get :edit, {:id => link.to_param}
+        expect(response).to redirect_to(login_url)
+      end
     end
   end
 
   describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Link" do
-        expect {
+    context "when logged in" do
+      context "with valid params" do
+        it "creates a new Link" do
+          expect {
+            post :create, {:link => valid_attributes}, valid_session
+          }.to change(Link, :count).by(1)
+        end
+
+        it "assigns a newly created link as @link" do
           post :create, {:link => valid_attributes}, valid_session
-        }.to change(Link, :count).by(1)
+          expect(assigns(:link)).to be_a(Link)
+          expect(assigns(:link)).to be_persisted
+        end
+
+        it "redirects to the created link" do
+          post :create, {:link => valid_attributes}, valid_session
+          expect(response).to redirect_to(Link.last)
+        end
       end
 
-      it "assigns a newly created link as @link" do
-        post :create, {:link => valid_attributes}, valid_session
-        expect(assigns(:link)).to be_a(Link)
-        expect(assigns(:link)).to be_persisted
-      end
+      context "with invalid params" do
+        it "assigns a newly created but unsaved link as @link" do
+          post :create, {:link => invalid_attributes}, valid_session
+          expect(assigns(:link)).to be_a_new(Link)
+        end
 
-      it "redirects to the created link" do
-        post :create, {:link => valid_attributes}, valid_session
-        expect(response).to redirect_to(Link.last)
+        it "re-renders the 'new' template" do
+          post :create, {:link => invalid_attributes}, valid_session
+          expect(response).to render_template("new")
+        end
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved link as @link" do
-        post :create, {:link => invalid_attributes}, valid_session
-        expect(assigns(:link)).to be_a_new(Link)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:link => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
+    context "when not logged in" do
+      it "redirects to login" do
+        post :create, {:link => valid_attributes}
+        expect(response).to redirect_to(login_url)
       end
     end
   end
 
   describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {{short_url: "google-home"}}
+    let(:new_attributes) {{short_url: "google-home"}}
 
-      it "updates the requested link" do
-        link = Link.create! valid_attributes
-        put :update, {:id => link.to_param, :link => new_attributes}, valid_session
-        link.reload
-        expect(link.short_url).to eq("google-home")
+    context "when logged in" do
+      context "with valid params" do
+
+        it "updates the requested link" do
+          link = Link.create! valid_attributes
+          put :update, {:id => link.to_param, :link => new_attributes}, valid_session
+          link.reload
+          expect(link.short_url).to eq("google-home")
+        end
+
+        it "assigns the requested link as @link" do
+          link = Link.create! valid_attributes
+          put :update, {:id => link.to_param, :link => valid_attributes}, valid_session
+          expect(assigns(:link)).to eq(link)
+        end
+
+        it "redirects to the link" do
+          link = Link.create! valid_attributes
+          put :update, {:id => link.to_param, :link => valid_attributes}, valid_session
+          expect(response).to redirect_to(link)
+        end
       end
 
-      it "assigns the requested link as @link" do
-        link = Link.create! valid_attributes
-        put :update, {:id => link.to_param, :link => valid_attributes}, valid_session
-        expect(assigns(:link)).to eq(link)
-      end
+      context "with invalid params" do
+        it "assigns the link as @link" do
+          link = Link.create! valid_attributes
+          put :update, {:id => link.to_param, :link => invalid_attributes}, valid_session
+          expect(assigns(:link)).to eq(link)
+        end
 
-      it "redirects to the link" do
-        link = Link.create! valid_attributes
-        put :update, {:id => link.to_param, :link => valid_attributes}, valid_session
-        expect(response).to redirect_to(link)
+        it "re-renders the 'edit' template" do
+          link = Link.create! valid_attributes
+          put :update, {:id => link.to_param, :link => invalid_attributes}, valid_session
+          expect(response).to render_template("edit")
+        end
       end
     end
 
-    context "with invalid params" do
-      it "assigns the link as @link" do
+    context "when not logged in" do
+      it "redirects to login" do
         link = Link.create! valid_attributes
-        put :update, {:id => link.to_param, :link => invalid_attributes}, valid_session
-        expect(assigns(:link)).to eq(link)
-      end
-
-      it "re-renders the 'edit' template" do
-        link = Link.create! valid_attributes
-        put :update, {:id => link.to_param, :link => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
+        put :update, {:id => link.to_param, :link => new_attributes}
+        expect(response).to redirect_to(login_url)
       end
     end
   end
 
   describe "DELETE #destroy" do
-    it "destroys the requested link" do
-      link = Link.create! valid_attributes
-      expect {
+    context "when logged in" do
+      it "destroys the requested link" do
+        link = Link.create! valid_attributes
+        expect {
+          delete :destroy, {:id => link.to_param}, valid_session
+        }.to change(Link, :count).by(-1)
+      end
+
+      it "redirects to the links list" do
+        link = Link.create! valid_attributes
         delete :destroy, {:id => link.to_param}, valid_session
-      }.to change(Link, :count).by(-1)
+        expect(response).to redirect_to(links_url)
+      end
     end
 
-    it "redirects to the links list" do
-      link = Link.create! valid_attributes
-      delete :destroy, {:id => link.to_param}, valid_session
-      expect(response).to redirect_to(links_url)
+    context "when not logged in" do
+      it "redirects to login" do
+        link = Link.create! valid_attributes
+        delete :destroy, {:id => link.to_param}
+        expect(response).to redirect_to(login_url)
+      end
     end
   end
 end
