679e7a992640747fd6308253b6ad4017762013ae
Sam Blackman <sblackman@gmail.com>
2016-06-14 13:39:55 -0400

    remove comments and json responses

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -18,28 +18,20 @@ class LinksController < ApplicationController
     end
   end
 
-  # GET /links
-  # GET /links.json
   def index
     @links = current_user.links
   end
 
-  # GET /links/1
-  # GET /links/1.json
   def show
   end
 
-  # GET /links/new
   def new
     @link = Link.new
   end
 
-  # GET /links/1/edit
   def edit
   end
 
-  # POST /links
-  # POST /links.json
   def create
     @link = Link.new(link_params)
 
@@ -54,37 +46,25 @@ class LinksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
   def update
-    respond_to do |format|
       if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
+        redirect_to @link, notice: 'Link was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
+        render :edit
       end
     end
   end
 
-  # DELETE /links/1
-  # DELETE /links/1.json
   def destroy
     @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to links_url, notice: 'Link was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
       params.require(:link).permit(:full_url, :short_url).merge(user: current_user)
     end
