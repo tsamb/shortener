afd3cf467a746b7431da6f18aec0de1a055361d6
Sam Blackman <sblackman@gmail.com>
2016-06-14 13:36:32 -0400

    clean up tests to basic state

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,8 @@ class Link < ActiveRecord::Base
   has_many :requests
   belongs_to :user
 
+  validates :full_url, :short_url, presence: true
+
   def click_count
     requests.count
   end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:link) { Link.create(full_url: "http://www.google.com", short_url: "thegoog") }
+
+  describe "link" do
+    it "has a short_url" do
+      expect(link.short_url).to eq("thegoog")
+    end
+  end
 end
