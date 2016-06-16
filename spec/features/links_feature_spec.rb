9162e9391c8526719b00077f9fcbbe22716a1c77
Sam Blackman <sblackman@gmail.com>
2016-06-16 11:43:21 -0400

    write feature spec for link edit

--- spec/features/links_feature_spec.rb
+++ spec/features/links_feature_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Links resource", type: :feature do
+  let!(:user) { User.create!(:username => 'sam', :password => 'password') }
+
   before :each do
-    User.create!(:username => 'sam', :password => 'password')
     visit '/login'
     within('form') do
       fill_in 'Username', :with => 'sam'
@@ -16,8 +17,8 @@ RSpec.feature "Links resource", type: :feature do
       scenario "User creates a link" do
         click_link 'New Link'
         within('form') do
-          fill_in 'Full url', :with => 'goog'
-          fill_in 'Short url', :with => 'http://www.google.com'
+          fill_in 'Short url', :with => 'goog'
+          fill_in 'Full url', :with => 'http://www.google.com'
         end
         click_button 'Create Link'
         
@@ -29,7 +30,80 @@ RSpec.feature "Links resource", type: :feature do
     end
 
     context "with invalid input" do
+      scenario "User creates a link with neither url filled in" do
+        click_link 'New Link'
+        click_button 'Create Link'
 
+        expect(page).to_not have_content 'Link was successfully created.'
+        expect(page).to have_content "Full url can't be blank"
+        expect(page).to have_content "Short url can't be blank"
+      end
+
+      scenario "User creates a link without filling in the full url" do
+        click_link 'New Link'
+        within('form') do
+          fill_in 'Short url', :with => 'exists'
+        end
+        click_button 'Create Link'
+        
+        expect(page).to_not have_content 'Link was successfully created.'
+        expect(page).to have_content "Full url can't be blank"
+      end
+
+      scenario "User creates a link without filling in the short url" do
+        click_link 'New Link'
+        within('form') do
+          fill_in 'Full url', :with => 'http://www.google.com'
+        end
+        click_button 'Create Link'
+        
+        expect(page).to_not have_content 'Link was successfully created.'
+        expect(page).to have_content "Short url can't be blank"
+      end
+    end
+  end
+
+  feature "Editing links" do
+    let(:link) { Link.create!(full_url: 'http://www.google.com', short_url: 'goog', user: user) }
+
+    context "with valid input" do
+      scenario "User edits a link" do
+        visit link_path(link)
+        click_link 'Edit'
+
+        expect(find_field('Full url').value).to have_content 'http://www.google.com'
+        expect(find_field('Short url').value).to have_content 'goog'
+
+        within('form') do
+          fill_in 'Full url', :with => 'googlemaps'
+          fill_in 'Short url', :with => 'http://maps.google.com'
+        end
+        click_button 'Update Link'
+
+        expect(page).to have_content 'Link was successfully updated.'
+        expect(page).to have_content 'googlemaps'
+        expect(page).to have_content 'http://maps.google.com'
+      end
+    end
+
+    context "with invalid input" do
+      scenario "User creates a link" do
+        visit link_path(link)
+        click_link 'Edit'
+
+        expect(find_field('Full url').value).to have_content 'http://www.google.com'
+        expect(find_field('Short url').value).to have_content 'goog'
+
+        within('form') do
+          fill_in 'Full url', :with => ''
+          fill_in 'Short url', :with => ''
+        end
+        click_button 'Update Link'
+        
+        expect(page).to_not have_content 'Link was successfully updated.'
+        expect(page).to have_content "Full url can't be blank"
+        expect(page).to have_content "Short url can't be blank"
+      end
     end
   end
 end
