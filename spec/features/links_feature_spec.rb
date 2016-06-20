e894eb5dc69ccce3d0c28459230dbb34111a544c
Sam Blackman <sblackman@gmail.com>
2016-06-20 18:24:33 -0400

    implement selenium delete link test

--- spec/features/links_feature_spec.rb
+++ spec/features/links_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Links resource", type: :feature do
+RSpec.feature "FEATURE: CRUDing links", type: :feature do
   let!(:user) { User.create!(:username => 'sam', :password => 'password') }
 
   before :each do
@@ -106,4 +106,23 @@ RSpec.feature "Links resource", type: :feature do
       end
     end
   end
+
+  feature "Deleting links", js: true do
+    let!(:link) { Link.create!(full_url: 'http://www.google.com', short_url: 'goog', user: user) }
+
+    scenario "User deletes a link" do
+      visit links_path
+      # $stderr.write 'Press enter to continue'
+      # $stdin.gets
+      expect(page).to have_content 'http://www.google.com'
+
+      accept_alert do
+        click_link 'Destroy'
+      end
+
+      expect(page).to have_content 'Link was successfully destroyed.'
+      expect(page).to_not have_content 'http://www.google.com'
+    end
+  end
+
 end
